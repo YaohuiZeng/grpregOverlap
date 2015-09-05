697af5e13bde6a0e2d45ae0e3918a8a7f8b2ea4d
Yaohui Zeng <yaohui-zeng@uiowa.edu>
2015-09-05 12:43:21 -0500

    Update cv.grpregOverlap.R

--- R/cv.grpregOverlap.R
+++ R/cv.grpregOverlap.R
@@ -1,12 +1,12 @@
 ## function: cross-validation
 # ------------------------------------------------------------------------------
-cv.overlap.grpreg <- function(X, y, group, ..., nfolds=10, seed, trace=FALSE) {
-  fit <- overlap.grpreg(X=X, y=y, group=group, ...)
+cv.grpregOverlap <- function(X, y, group, ..., nfolds=10, seed, trace=FALSE) {
+  fit <- grpregOverlap(X=X, y=y, group=group, ...)
   cvfit <- cv.grpreg(X = fit$X.latent, y = y, group = fit$grp.vec, ...,
                      nfolds = nfolds, seed = seed, 
                      trace = trace)
   cvfit$fit <- fit
-  val <- structure(cvfit, class = c('cv.overlap.grpreg', 'cv.grpreg'))
+  val <- structure(cvfit, class = c('cv.grpregOverlap', 'cv.grpreg'))
   val
 }
 # ------------------------------------------------------------------------------
